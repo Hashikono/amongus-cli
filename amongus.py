6c94b675862a96067db88b0f1dc1b25484dc32f8
hashikono <hashikono@outlook.com>
2026-02-28 20:58:18 -0800

    Replaced map and attempted and attempted to try and make viewpoints through controls

--- amongus.py
+++ amongus.py
@@ -1,47 +1,57 @@
 #Among us CLI 8x8 grid - autoupdating
 mainMap = [
-    ["-","-","-","-","-","-","-","-"],
-    ["-","-","-","-","-","-","-","+"],
-    [" "," "," "," "," "," "," ","|"],
-    [" "," "," "," "," "," "," ","|"],
-    [" "," "," "," "," "," "," ","|"],
-    [" "," "," "," "," "," "," ","|"],
-    [" "," "," "," "," "," "," ","|"],
-    ["+","🟥","+","-","-","-","-","+"]
-    ]
+    list("+----------------------+----------------------+----------------------+"),
+    list("|C                     |                      |                      |"),
+    list("|                      |                      |                      |"),
+    list("|   O    O    O    O   +-----------+----------+                      |"),
+    list("|                               /                                     "),
+    list("+--------+-----------+         /                                     |"),
+    list("        |                     /        +-----------------------------+"),
+    list("        |                                                             "),
+    list("+-------+----------+-------+           +-----------+-----------------+"),
+    list("|                                                                    |"),
+    list("+---   ----+-----   --+-------+                   +------------------+"),
+    list("           |          |                                              |"),
+    list("+--   -----+----   ---+-------------------   ---------+----   -------+"),
+    list("|                             |                       |              |"),
+    list("+-----------------------------+-----------+-      ---+---------------+"),
+    list("                                          |          |----------------"),
+    list("                                          +--     ---+---------------+"),
+    list("                                          |                      |----"),
+    list("                                          +-----    --+---------------"),
+    list("                              +------------+          |--------------+"),
+    list("                              |                                      |"),
+    list("                              +-------------------+------------------+"),
+]
 
-y = 7
+y = 1
 x = 1
-cont = True;
-
-
+cont = True
 while cont:
     #prints out map
-    for level in range(len(mainMap)):
-        for dash in mainMap[level]:
-            print(dash, end=" ")
+    for ya in range(8):
+        for xa in range(8):
+            print(mainMap[ya][xa], end=" ")
         print("")
     #waiting for input
     movement = input()
-    
     #basic movement
     if movement == "w" and y > 0 and mainMap[y-1][x] == " ":
-        mainMap[y-1][x] =  mainMap[y][x]
+        mainMap[y-1][x] = mainMap[y][x]
         mainMap[y][x] = " "
         y -= 1
     elif movement == "a" and x > 0 and mainMap[y][x-1] == " ":
-        mainMap[y][x-1] =  mainMap[y][x]
+        mainMap[y][x-1] = mainMap[y][x]
         mainMap[y][x] = " "
         x -= 1
     elif movement == "s" and y < 7 and mainMap[y+1][x] == " ":
-        mainMap[y+1][x] =  mainMap[y][x]
+        mainMap[y+1][x] = mainMap[y][x]
         mainMap[y][x] = " "
         y += 1
     elif movement == "d" and x < 7 and mainMap[y][x+1] == " ":
-        mainMap[y][x+1] =  mainMap[y][x]
+        mainMap[y][x+1] = mainMap[y][x]
         mainMap[y][x] = " "
         x += 1
     elif movement == "q":
         cont = False
-    
 
