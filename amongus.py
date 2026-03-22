d23b047b1e52ffb746caa16e01c9cb3d83cc783b
hashikono <hashikono@outlook.com>
2026-03-21 21:03:58 -0700

    added the code for constant update/testing so it feels more like a real game ... pressing enter is annoying

--- amongus.py
+++ amongus.py
@@ -1,7 +1,95 @@
+#Imports: ANSI and terminal helper to allow for continual inputs
+import sys
+import time
+import msvcrt
+import ctypes
+
+def enable_windows_ansi():
+    #initializing a workable variable
+    kernel32 = ctypes.windll.kernel32
+    # -11 for this is the thing that allows us to control terinal outputs
+    hStdout = kernel32.GetStdHandle(-11)
+    #allows us to keep the updated mode/version
+    mode = ctypes.c_uint32()
+    #trying to read the current mode (small test)
+    if kernel32.GetConsoleMode(hStdout, ctypes.byref(mode)) == 0:
+        return False
+    
+    #establishing mode
+    #0x0004 = terminal processes
+    new_mode = mode.value | 0x0004
+    
+    #actual application
+    if kernel32.SetConsoleMode(hStdout, new_mode) == 0:
+        return False
+    return True
+
+def ansi_clear_screen():
+    # \x1b = ESC character
+    # [2J  = clear the whole screen
+    # [H   = move cursor to home position (row 1, col 1)
+    sys.stdout.write("\x1b[2J\x1b[H")
+
+def ansi_move_cursor(row, col):
+    #chucks the cursor somewhere
+    sys.stdout.write(f"\x1b[{row};{col}H")
+
+def ansi_hide_cursor():
+    sys.stdout.write("\x1b[?25l")
+
+def ansi_show_cursor():
+    sys.stdout.write("\x1b[?25h")
+
+#forces our inputs to appear on screen
+def flush():
+    sys.stdout.flush()
+
+#if you want to use arrow keys (converting arrow keys to wasd)
+def poll_key():
+    # If no key is waiting, return None immediately.
+    # This is what makes the input NON-BLOCKING.
+    if not msvcrt.kbhit():
+        return None
+
+    # Read one key press as raw bytes
+    ch = msvcrt.getch()
+
+    # Arrow keys are special on Windows:
+    # first byte is b'\x00' or b'\xe0'
+    # second byte tells which arrow it was
+    if ch in (b"\x00", b"\xe0"):
+        ch2 = msvcrt.getch()
+        code = ch2[0]
+
+        # Convert arrows into the same controls as WASD
+        if code == 72:   # up arrow
+            return "w"
+        elif code == 80: # down arrow
+            return "s"
+        elif code == 75: # left arrow
+            return "a"
+        elif code == 77: # right arrow
+            return "d"
+        return None
+
+    # For normal keys, decode the byte into text
+    try:
+        k = ch.decode("utf-8", errors="ignore").lower()
+    except:
+        return None
+
+    # Only allow keys we care about
+    if k in ["w", "a", "s", "d", "q"]:
+        return k
+
+    return None
+
+
+
 #Among us CLI 8x8 grid - autoupdating
 mainMap = [
     list("+----------------------+----------------------+----------------------+"),
-    list("|C                     |                      |                      |"),
+    list("|                      |                      |                      |"),
     list("|                      |                      |                      |"),
     list("|   O    O    O    O   +-----------+----------+                      |"),
     list("|                               /                                     "),
@@ -33,6 +121,8 @@ cont = True
 view_x = 0
 view_y = 0
 
+#variable for player
+mainMap[y][x] = "P"
 
 def viewpoint(direction):
     #refers global variables and ensures changes made to them
@@ -50,41 +140,59 @@ def viewpoint(direction):
     elif x > view_x + 4:
         view_x = min(len(mainMap[0]) - 8, x - 4)
 
-
-while cont:
+#applies terminal update to the game
+def draw_view():
+    ansi_move_cursor(1,1)
     #initialize 8x8 viewpoint
-    print("&&&&&&&&&&&&&&&&&&&&")
     for ya in range(view_y, view_y + 8):
+        row = []
         for xa in range(view_x, view_x + 8):
-            print(mainMap[ya][xa], end=" ")
-        print()
+            row.append(mainMap[ya][xa])
+        # .ljust(60) makes sure old longer lines get overwritten (erasing old print statements)
+        print(" ".join(row).ljust(60))
+    flush()
+    
+enable_windows_ansi()
+ansi_clear_screen()
+ansi_hide_cursor()
 
-    movement = input()
+try:
+    while cont:
+        #updates the view
+        draw_view()
+        #check for arrow keys
+        movement = poll_key()
 
-    #check: movement type | we can move | empty space
-    if movement == "w" and y > 0 and mainMap[y - 1][x] == " ":
-        mainMap[y - 1][x] = mainMap[y][x]
-        mainMap[y][x] = " "
-        y -= 1
-        viewpoint("up")
 
-    elif movement == "a" and x > 0 and mainMap[y][x - 1] == " ":
-        mainMap[y][x - 1] = mainMap[y][x]
-        mainMap[y][x] = " "
-        x -= 1
-        viewpoint("left")
+        #check: movement type | we can move | empty space
+        if movement == "w" and y > 0 and mainMap[y - 1][x] == " ":
+            mainMap[y - 1][x] = mainMap[y][x]
+            mainMap[y][x] = " "
+            y -= 1
+            viewpoint("up")
 
-    elif movement == "s" and y < len(mainMap) - 1 and mainMap[y + 1][x] == " ":
-        mainMap[y + 1][x] = mainMap[y][x]
-        mainMap[y][x] = " "
-        y += 1
-        viewpoint("down")
+        elif movement == "a" and x > 0 and mainMap[y][x - 1] == " ":
+            mainMap[y][x - 1] = mainMap[y][x]
+            mainMap[y][x] = " "
+            x -= 1
+            viewpoint("left")
 
-    elif movement == "d" and x < len(mainMap[0]) - 1 and mainMap[y][x + 1] == " ":
-        mainMap[y][x + 1] = mainMap[y][x]
-        mainMap[y][x] = " "
-        x += 1
-        viewpoint("right")
+        elif movement == "s" and y < len(mainMap) - 1 and mainMap[y + 1][x] == " ":
+            mainMap[y + 1][x] = mainMap[y][x]
+            mainMap[y][x] = " "
+            y += 1
+            viewpoint("down")
 
-    elif movement == "q":
-        cont = False
+        elif movement == "d" and x < len(mainMap[0]) - 1 and mainMap[y][x + 1] == " ":
+            mainMap[y][x + 1] = mainMap[y][x]
+            mainMap[y][x] = " "
+            x += 1
+            viewpoint("right")
+
+        elif movement == "q":
+            cont = False
+
+        time.sleep(0.03)
+finally:
+    ansi_show_cursor()
+    ansi_move_cursor(15, 1)
